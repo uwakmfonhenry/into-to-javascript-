e0db9d8934bbc9ed58ecbc2a65c1e0ddc07de2fb
Uwakmfon Henry <uwakyz@gmail.com>
2024-10-12 14:39:11 +0100

    first commit

---
+++ objects.js
@@ -0,0 +1,43 @@
+// javascript Object literal
+
+const person = {
+    firstName: "Henry",
+    lastName: "Uwakmfon",
+    age: 90,
+    gender: "female",
+    isVerified: true,
+    login: {
+        userName: "",
+        email: "",    
+},
+    images: ["file.png", "file2.png"],
+    Videos: [
+        {
+            url: "file.mp4",
+            size: 1024,
+            format: "mp4",
+}
+]
+};
+
+const car ={
+    brand: "Kia",
+    name: "Picanto",
+    model: "",
+    year: "2010",
+    color: "black",
+    isForSale: "true",
+    sold: "false",
+    images: ["file.png"],
+    gear: "automatic",
+    noofDoors:4,
+    types:""
+};
+
+const user ={
+    firstName:"",
+    lastName: "",
+    age: 90,
+    gender: "",
+    email: "",
+}
---
+++ test.js
@@ -0,0 +1,15 @@
+const a = "20";
+const b = 5;
+let sum;
+
+sum = a+b;
+
+// this holds the first name
+const firstName = "Tari";
+const lastName = "Philip";
+const fullName = firstName + " " + lastName;
+
+console.log(sum);
+console.log(fullName);
+// typscript adds fancy to it
+//concatination a statement we use to join twostrings together e.g "20" + b =20b
